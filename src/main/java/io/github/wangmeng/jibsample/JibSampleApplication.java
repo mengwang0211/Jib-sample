532707088d41c3a5d4e3055adeb46d2b2d1d38d7
Meng Wang <wmlucas@163.com>
2019-02-15 12:51:48 +0800

    init project

---
+++ src/main/java/io/github/wangmeng/jibsample/JibSampleApplication.java
@@ -0,0 +1,14 @@
+package io.github.wangmeng.jibsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JibSampleApplication.class, args);
+    }
+
+}
+
